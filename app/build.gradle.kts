abad70ea1757bf938641fd7f89276bf87d1bc4ce
Amanpreet Kaur <33446281+kauramanp@users.noreply.github.com>
2025-02-28 23:15:30 +0530

    Http Logging interceptor added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.3.0")
     implementation ("com.github.bumptech.glide:glide:4.13.2")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
 
 }
--- app/src/main/java/com/aman/retrofitapplication/RetrofitInstance.kt
+++ app/src/main/java/com/aman/retrofitapplication/RetrofitInstance.kt
@@ -1,15 +1,23 @@
 package com.aman.retrofitapplication
 
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import okhttp3.logging.HttpLoggingInterceptor
 
 object RetrofitInstance {
+
+    val logging = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+    val client = OkHttpClient.Builder().addInterceptor(logging).build()
+
     val api : RetrofitInterface by lazy {
         Retrofit.Builder()
             .baseUrl("https://gorest.co.in/public/v2/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(RetrofitInterface::class.java)
     }
